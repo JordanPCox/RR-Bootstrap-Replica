f6731c32f7744cd2212c0876c385cb2d6de62988
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-02-28 16:39:08 -0500

    Added background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import TopBar from './components/TopBar'
+import Background from './components/Background'
+import StoreListing from './components/StoreListing'
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <Background />
+      <StoreListing />
     </div>
   );
 }
